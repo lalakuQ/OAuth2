4e3e331f236d98d7caca976169c1f2226b1ac9dd
lalakuQ <vdyugay2004@gmail.com>
2024-07-30 02:35:21 +0500

    refactroring

--- oauth2/core/utils.py
+++ oauth2/core/utils.py
@@ -1,5 +1,5 @@
-from .constants import OAUTH_ENDPOINTS
+from .constants import GOOGLE_OAUTH_ENDPOINTS
 
 
 def get_google_user_details_url(access_token):
-    return f'{OAUTH_ENDPOINTS['info_url']}?access_token={access_token}'
+    return f'{GOOGLE_OAUTH_ENDPOINTS['info_url']}?access_token={access_token}'
